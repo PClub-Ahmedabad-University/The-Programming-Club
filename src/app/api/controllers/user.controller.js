ba87c33b71deddbfc957137d05ff3b54032f5b62
Drumil <drumilbhati5@gmail.com>
2025-05-31 12:27:09 +0530

    Implement user registration and login functionality with JWT authentication

---
+++ src/app/api/controllers/user.controller.js
@@ -0,0 +1,29 @@
+import User from '@/models/user.model.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import connectDB from '../lib/db';
+
+const secret = process.env.secret;
+
+export const registerUser = async(data) => {
+    await connectDB();
+    const hashedPassword = await bcrypt.hash(data.passowrd, 10);
+    const user = await User.create({ ...data, passowrd: hashedPassword });
+    return user;
+}
+
+export const loginUser = async(data) => {
+    await connectDB();
+    const user = await User.findOne({ email: data.email });
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(user.passowrd, data.passowrd);
+    if (!isMatch) {
+        throw new Error('Invalid credentials');
+    }
+
+    const token = jwt.sign({ id: user._id }, secret, { expiresIn: '7d' })
+    return { token, user };
+}

8d40187192842a8c3ef19eb422ce526638a42420
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-05-31 15:27:12 +0530

    Add href to navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,46 +1,60 @@
 "use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import {React, useState} from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import { React, useState } from "react";
 import "../Styles/Navbar.css";
-import DrawerIcon from '../Client Components/DrawerIcon';
+import DrawerIcon from "../Client Components/DrawerIcon";
 import { AnimatePresence, motion } from "motion/react";
-import Sidebar from '../Client Components/Sidebar';
+import Sidebar from "../Client Components/Sidebar";
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const navLinks = [
-    {name: "Home"},
-    {name: "Past Events"},
-    {name: "Join Us"},
-    {name: "Contact Us"},
+    { name: "Home" },
+    { name: "Past Events" },
+    { name: "Join Us" },
+    { name: "Contact Us" },
   ];
 
   return (
     <>
-    <header className="w-full sticky top-0 backdrop-blur-2xl px-8 md:px-16 lg:px-25">
+      <header className="z-30 w-full sticky top-0 backdrop-blur-2xl px-8 md:px-16 lg:px-25">
         {/* Navbar */}
-        <nav className='nav flex justify-between items-center font-inter'>
-            <Link href="/" className="logo">
-                <Image src="/logo.png" alt='PClub Logo' height={100} width={100} className='h-30 w-15'/>
-            </Link>
-            <ul className="nav-items max-md:hidden flex gap-8 text-white">
-                {navLinks.map((item,index) => (
-                    <Link key={index} href="/" className='nav-links relative'>{item.name}</Link>
-                ))}
-            </ul>
-            <DrawerIcon onClick={() => setSidebarOpen(true)}/>
+        <nav className="nav flex justify-between items-center font-inter">
+          <Link href="/" className="logo">
+            <Image
+              src="/logo.png"
+              alt="PClub Logo"
+              height={100}
+              width={100}
+              className="h-30 w-15"
+            />
+          </Link>
+          <ul className="nav-items max-md:hidden flex gap-8 text-white">
+            {navLinks.map((item, index) => (
+              <Link
+                key={index}
+                href={
+                  item.name === "Home"
+                    ? "/"
+                    : `/${item.name.toLowerCase().replace(/\s+/g, "-")}`
+                }
+                className="nav-links relative"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+          <DrawerIcon onClick={() => setSidebarOpen(true)} />
         </nav>
-    </header>
-    <AnimatePresence>
-        {sidebarOpen && (
-            <Sidebar setSidebarOpen={setSidebarOpen}/>
-        )}
-    </AnimatePresence>
+      </header>
+      <AnimatePresence>
+        {sidebarOpen && <Sidebar setSidebarOpen={setSidebarOpen} />}
+      </AnimatePresence>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

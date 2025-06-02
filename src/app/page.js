130651ebc6f0f91ab9cf670853fe0f218c2121ec
Kushal Rathod <kushalrathod044@gmail.com>
2025-06-02 11:00:03 +0530

    Clun Highlights Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
-import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import ClubHighlights from "./Components/ClubHighlights";
 
 export default function Home() {
   return (
     <div className="app w-screen relative">
       <Hero/>
+      <ClubHighlights/>
     </div>
   );
 }

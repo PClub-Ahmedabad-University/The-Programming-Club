8cfe3023d3fe4c2c1836dce448f0a32d227b544b
Jay-1409 <jay_shah@outlook.in>
2025-06-12 12:36:54 +0530

    lets fix it again -_-

--- src/app/api/hook/route.js
+++ src/app/api/hook/route.js
@@ -1,10 +1,13 @@
 import { Submission } from '../models/submission.model';
 import connectDB from '../lib/db';
-
 export async function POST(req) {
   try {
     await connectDB();
-    const data = await req.json();
+    const formData = await req.formData();
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
+    }
     const submission = new Submission(data);
     await submission.save();
     return new Response(JSON.stringify({ status: 'success' }), { status: 200 });

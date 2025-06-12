d1e3ba0989e26094d3cce3c6fc13a5254a6067de
Jay-1409 <jay_shah@outlook.in>
2025-06-12 12:32:14 +0530

    fixed

--- src/app/api/hook/route.js
+++ src/app/api/hook/route.js
@@ -1,34 +1,11 @@
 import { Submission } from '../models/submission.model';
-import { IncomingForm } from 'formidable';
-import { Readable } from 'stream';
 import connectDB from '../lib/db';
-export const config = {
-  api: {
-    bodyParser: false, 
-  },
-};
-function parseFormData(req) {
-  const form = new IncomingForm();
-  return new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      else resolve({ fields, files });
-    });
-  });
-}
+
 export async function POST(req) {
   try {
     await connectDB();
-    const { fields } = await parseFormData(req);
-    if (fields.rawRequest) {
-      try {
-        const rawParsed = JSON.parse(fields.rawRequest);
-        fields.rawRequest = rawParsed;
-      } catch (e) {
-        console.warn('rawRequest is not valid JSON');
-      }
-    }
-    const submission = new Submission(fields);
+    const data = await req.json();
+    const submission = new Submission(data);
     await submission.save();
     return new Response(JSON.stringify({ status: 'success' }), { status: 200 });
   } catch (err) {

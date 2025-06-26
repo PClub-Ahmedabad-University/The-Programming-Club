78564cd5b5fa7fbf961f426c352ad61558998894
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-06-26 20:17:51 +0530

    Secured apis

--- src/app/WMC/page.jsx
+++ src/app/WMC/page.jsx
@@ -1,13 +1,11 @@
 "use client";
 import React, { useRef } from "react";
-// import ComingSoon from "../Components/ComingSoon";
 
 const WMCPage = () => {
     const iframeRef = useRef(null);
 
     return (
         <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
-            {/* <ComingSoon /> */}
             <iframe
                 ref={iframeRef}
                 src="/WMC/index.html"

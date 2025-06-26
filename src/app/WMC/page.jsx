29477ff4abeeb5580aaa94eff5b5456c770e6e35
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2025-06-26 11:48:27 +0530

    Hide WMC page

--- src/app/WMC/page.jsx
+++ src/app/WMC/page.jsx
@@ -1,17 +1,19 @@
 "use client";
 import React, { useRef } from "react";
+import ComingSoon from "../components/ComingSoon";
 
 const WMCPage = () => {
     const iframeRef = useRef(null);
 
     return (
         <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
-            <iframe
+            <ComingSoon />
+            {/* <iframe
                 ref={iframeRef}
                 src="/WMC/index.html"
                 title="WMC Source Page"
                 style={{ width: "100%", height: "100%", border: "none" }}
-            />
+            /> */}
         </div>
     );
 };

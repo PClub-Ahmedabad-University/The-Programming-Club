8e0999baae3a7a0630ae0a36cf5b1f018dc89ba2
Kushal Rathod <kushalrathod044@gmail.com>
2025-07-29 12:00:13 +0530

    Final Comments Changes

--- src/app/blogs/[blogId]/commentbox.jsx
+++ src/app/blogs/[blogId]/commentbox.jsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import { formatDistance } from "date-fns";
 import dynamic from 'next/dynamic';
+import { getToken, getUserIdFromToken } from '@/lib/auth';
+import { useRouter } from 'next/navigation';
 
 const Tiptap = dynamic(() => import('@/components/Tiptap'), {
   ssr: false,
@@ -17,6 +19,8 @@ const CommentBox = ({ data, blogData, blogId, type, user, getComments }) => {
   const [hover, setHover] = useState(false);
   const [openBox, setOpenBox] = useState(false);
   const [commentData, setCommentData] = useState("");
+  const [isUser, setUser] = useState(false);
+  const router = useRouter();
 
   const handleSubmit = async () => {
     const plainText = new DOMParser().parseFromString(commentData, 'text/html').body.textContent || "";
@@ -37,16 +41,32 @@ const CommentBox = ({ data, blogData, blogId, type, user, getComments }) => {
     }
   }
 
+  const handleClick = () => {
+    const token = getToken();
+    if (token) {
+        const currentUserId = getUserIdFromToken(token);
+        if (currentUserId) {
+            setUser(true);
+            setOpenBox(true);
+        } else {
+            console.error('Failed to get user info from token');
+        }
+    } else {
+        console.error('No token found in localStorage');
+        router.push("/users/login");
+    }
+  }
+
   return (
     <div
-      className={`comment-box w-full flex items-start gap-4 font-inter 
-        ${type === "nested" ? "mt-4 mb-2 pl-6 border-l-[2px] border-gray-700" : "mt-6 mb-8"}`}
+      className={`comment-box w-full flex items-start gap-4 font-inter overflow-x-auto
+        ${type === "nested" ? "mt-4 mb-2 pl-6 border-l-[2px] border-gray-700 max-sm:pl-3" : "mt-3 mb-8"}`}
     >
       <div className="flex flex-col h-fit items-start">
         <div
           className={`circle ${
             type === "nested" ? "h-8 w-8 text-sm" : "h-12 w-12 text-base"
-          } flex justify-center items-center bg-black text-white rounded-full font-bold relative`}
+          } flex justify-center items-center bg-black text-white rounded-full font-bold relative max-sm:h-8 max-sm:w-8 max-sm:text-sm`}
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
@@ -64,8 +84,15 @@ const CommentBox = ({ data, blogData, blogId, type, user, getComments }) => {
           {data.content}
         </p>
         <div className={`box-bottom flex items-center mt-1 gap-5 ${openBox && `mb-3`}`}>
-            <p className='text-[14px] underline cursor-pointer' onClick={() => setOpenBox(true)}>Reply</p>
-            <p>{formatDistance(new Date(), data.createdAt)}</p>
+            <p className='text-[14px] underline cursor-pointer' onClick={handleClick}>Reply</p>
+            <p>
+              {
+                formatDistance(new Date(), data.createdAt)
+                .replace("about","")
+                .replace("hours", "hrs")
+                .replace("minutes","min")
+              }
+            </p>
         </div>
 
         {/* Kept here to position it better */}
--- src/app/blogs/[blogId]/comments.jsx
+++ src/app/blogs/[blogId]/comments.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { FaComments } from "react-icons/fa6";
 import CommentBox from './commentbox';
 import dynamic from 'next/dynamic';
+import { getToken, getUserIdFromToken } from '@/lib/auth';
 import { useRouter } from 'next/navigation';
 
 const Tiptap = dynamic(() => import('@/components/Tiptap'), {
@@ -19,8 +20,12 @@ const Comments = ({ blogData, blogId, user }) => {
     const [allComments, setAllComments] = useState([]);
     const [openBox, setOpenBox] = useState(false);
     const [commentData, setCommentData] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [isUser, setUser] = useState(false);
+    const router = useRouter();
 
     const getComments = async () => {
+        setLoading(true);
         try {
             const res = await fetch(`/api/blog/${blogId}/comments/get`);
             if(res.status != 200) {
@@ -29,6 +34,7 @@ const Comments = ({ blogData, blogId, user }) => {
             }
             const comments = await res.json();
             setAllComments(comments);
+            setLoading(false);
         } catch (error) {
             console.error("Error fetching comments", error);
         }
@@ -56,14 +62,30 @@ const Comments = ({ blogData, blogId, user }) => {
         }
     }
 
+    const handleClick = () => {
+        const token = getToken();
+        if (token) {
+            const currentUserId = getUserIdFromToken(token);
+            if (currentUserId) {
+                setUser(true);
+                setOpenBox(true);
+            } else {
+                console.error('Failed to get user info from token');
+            }
+        } else {
+            console.error('No token found in localStorage');
+            router.push("/users/login");
+        }
+    }
+
   return (
-        <div className="comments-section w-full font-inter px-12 pb-5">
+        <div className="comments-section w-full font-inter px-12 pb-5 max-sm:px-5">
             <div className="section-top w-full flex justify-between items-center py-5">
-                <div className="section-left flex items-center gap-5">
-                    <FaComments size={45}/>
-                    <h1 className='text-2xl font-bold'>Comments</h1>
+                <div className="section-left flex items-center gap-5 max-sm:gap-3">
+                    <FaComments size={45} className='max-sm:h-8 max-sm:w-8'/>
+                    <h1 className='text-2xl font-bold max-sm:text-xl'>Comments</h1>
                 </div>
-                <div className="section-right flex items-center cursor-pointer" onClick={() => setOpenBox(true)}>
+                <div className="section-right flex items-center cursor-pointer hover:underline max-sm:text-sm" onClick={handleClick}>
                     Write a comment
                 </div>
             </div>
@@ -80,12 +102,19 @@ const Comments = ({ blogData, blogId, user }) => {
                 </>
             )}
             <div className="section-bottom w-full">
-                {allComments.length == 0 ? (
-                    <h1>No comments yet</h1>
+                {loading ? (
+                    <div className="box w-2/4 flex gap-3 mb-5">
+                        <div className="circle h-12 w-12 rounded-full bg-black flex justify-center items-center animate-pulse mt-1.5">L</div>
+                        <div className="content w-2/4 h-15 bg-gradient-to-r from-gray-700/80 to-gray-800/80 rounded-lg animate-pulse"></div>
+                    </div>
                 ) : (
-                    allComments.map((comment,index) => (
-                        <CommentBox key={index} data={comment} blogData={blogData} blogId={blogId} user={user} getComments={getComments} type="parent"/>
-                    ))
+                    allComments.length == 0 ? (
+                        <h1>No comments yet</h1>
+                    ) : (
+                        allComments.map((comment,index) => (
+                            <CommentBox key={index} data={comment} blogData={blogData} blogId={blogId} user={user} getComments={getComments} type="parent"/>
+                        ))
+                    )
                 )}
             </div>
         </div>

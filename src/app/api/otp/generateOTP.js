4458ebe66cbf9e0a9acf23708aa76fb2f2ebc250
Jay Shah <jay_shah@outlook.in>
2025-08-26 21:01:41 +0530

    generalizing the subject for mail under /api/otp/generateOTP

--- src/app/api/otp/generateOTP.js
+++ src/app/api/otp/generateOTP.js
@@ -6,7 +6,7 @@ const generateOTP = async (email) => {
 	const otp = crypto.randomInt(100000, 999999).toString();
 
 	await redis.setex(email, 300, otp);
-	sendMail(email, "PClub forgot password verification", `Your otp is ${otp}. It will expire in 5 minutes.`);
+	sendMail(email, "PClub OTP verification", `Your otp is ${otp}. It will expire in 5 minutes.`);
 	// await transporter.sendMail({
 	// 	from: process.env.EMAIL_USER,
 	// 	to: email,
